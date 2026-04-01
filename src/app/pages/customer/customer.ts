5c78e261036c6e90b367d242730346e324b6b357
Shag10 <shubhangagrawal1999@gmail.com>
2026-04-01 15:22:38 +0530

    UPD - Updated styles for customers component

--- src/app/pages/customer/customer.ts
+++ src/app/pages/customer/customer.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CustomerDetailsDialog } from '../customer-details-dialog/customer-details-dialog';
 import { CommonModule } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-customer',
-  imports: [CustomerDetailsDialog, CommonModule],
+  imports: [CommonModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
 export class CustomerComponent {
+   private modalService = inject(NgbModal);
+
+  openCustomerDetailsDialog() {
+    this.modalService.open(CustomerDetailsDialog);
+  }
 }
