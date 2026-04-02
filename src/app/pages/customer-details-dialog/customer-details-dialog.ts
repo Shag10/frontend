138f11e144efbb57ede9a57d3c254ea865b37028
Shag10 <shubhangagrawal1999@gmail.com>
2026-04-02 17:10:33 +0530

    Add - Added save customer Api

--- src/app/pages/customer-details-dialog/customer-details-dialog.ts
+++ src/app/pages/customer-details-dialog/customer-details-dialog.ts
@@ -1,9 +1,70 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-details-dialog',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './customer-details-dialog.html',
   styleUrl: './customer-details-dialog.css',
 })
-export class CustomerDetailsDialog {}
+export class CustomerDetailsDialog {
+  private httpClient = inject(HttpClient);
+  CustomerDetailsDto: any = [];
+
+  CustomerData = {
+    customerId: '',
+    customerName: '',
+    email: '',
+    phone: '',
+    address: '',
+    registrationDate: ''
+  }
+
+  ngOnInit() {
+    this.CustomerDetails();
+  }
+
+  CustomerDetails() {
+    let url = 'https://localhost:7284/api/customer';
+    this.httpClient.get(url).subscribe(data => {
+      this.CustomerDetailsDto = data;
+      console.log(this.CustomerDetailsDto);
+    }
+    );
+  }
+
+  onReset() {
+    this.CustomerData = {
+      customerId: '',
+      customerName: '',
+      email: '',
+      phone: '',
+      address: '',
+      registrationDate: ''
+    };
+  }
+  
+  onSubmit() {
+    let url = 'https://localhost:7284/api/customer';
+    let httpOptions = {
+      headers: {
+        Authorization: 'Shag123-auth' + localStorage.getItem('token'),
+        'Content-Type': 'application/json'
+      }
+    };
+    this.httpClient.put(url, this.CustomerData, httpOptions).subscribe(
+        {
+          next: v => console.log(v),
+          error: e => console.error(e),
+          complete: () => 
+          {
+            alert('Customer data updated successfully!' 
+              + JSON.stringify(this.CustomerData));
+            this.CustomerDetails();
+            }
+          }
+      );
+  }
+}
